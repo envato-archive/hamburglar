4ac5e7e46245f61f4aead30b9200dd502fe21791
Joshua Priddle <jpriddle@nevercraft.net>
2011-05-25 10:58:33 -0400

    Add Report#params and some other pending specs

--- lib/hamburglar/report.rb
+++ lib/hamburglar/report.rb
@@ -1,5 +1,9 @@
 module Hamburglar
+  # The Hamburglar::Report is the main class for generating fraud
+  # reports
   class Report
+    attr_reader :params
+
     def initialize(params = {})
       @params = params
     end
--- spec/hamburglar/report_spec.rb
+++ spec/hamburglar/report_spec.rb
@@ -1,12 +1,31 @@
 require 'spec_helper'
 
 describe Hamburglar::Report do
+  before :each do
+    @report = Hamburglar::Report.new(:foo => :bar)
+  end
+
   describe "#initialize" do
     it "sets @params" do
-      r = Hamburglar::Report.new(:foo => :bar)
-      p = r.instance_variable_get(:@params)
+      p = @report.instance_variable_get(:@params)
       p.should be_a Hash
       p.should have_key :foo
     end
+  end
+
+  describe "#params" do
+    it "returns @params hash" do
+      @report.params.should be_a Hash
+      raw_params = @report.instance_variable_get(:@params)
+      raw_params.should == @report.params
+    end
+  end
+
+  describe "#fraud?" do
+    it "returns true or false"
+  end
+
+  describe "#commit!" do
+    it "raises InvalidGateway unless Hamburglar.gateway is set"
   end
 end
