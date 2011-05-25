c7d9486e9938f8a76f5e9ef11c3ee4cacd217575
Joshua Priddle <jpriddle@nevercraft.net>
2011-05-25 16:27:55 -0400

    Add required params for MaxMind

--- lib/hamburglar/gateways/max_mind.rb
+++ lib/hamburglar/gateways/max_mind.rb
@@ -4,6 +4,9 @@ module Hamburglar
       # The MaxMind API URL
       # TODO: Switch to https
       api_url "http://minfraud2.maxmind.com/app/ccv2r"
+
+      # Required parameters for a MaxMind API call
+      required_params :i, :city, :region, :postal, :country, :license_key
     end
   end
 end
--- spec/hamburglar/gateways/max_mind_spec.rb
+++ spec/hamburglar/gateways/max_mind_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe Hamburglar::Gateways::MaxMind do
+  describe "required parameters" do
+    [:i, :city, :region, :postal, :country, :license_key].each do |p|
+      it { Hamburglar::Gateways::MaxMind.required_params.should include p }
+    end
+  end
+
   describe "::api_url" do
     it "has a valid API URL" do
       Hamburglar::Gateways::MaxMind.api_url.should match /https?:\/\/[\S]+/
