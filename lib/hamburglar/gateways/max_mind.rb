07aacd2b72ed633bf19c4b704fbd6ad3fdf4df2a
Joshua Priddle <jpriddle@nevercraft.net>
2011-05-25 15:52:13 -0400

    Setup URL for MaxMind

--- lib/hamburglar/gateways/max_mind.rb
+++ lib/hamburglar/gateways/max_mind.rb
@@ -1,6 +1,9 @@
 module Hamburglar
   module Gateways
-    class MaxMind
+    class MaxMind < Base
+      # The MaxMind API URL
+      # TODO: Switch to https
+      api_url "http://minfraud2.maxmind.com/app/ccv2r"
     end
   end
 end
--- spec/hamburglar/gateways/max_mind_spec.rb
+++ spec/hamburglar/gateways/max_mind_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe Hamburglar::Gateways::MaxMind do
-  it "has specs"
+  describe "::api_url" do
+    it "has a valid API URL" do
+      Hamburglar::Gateways::MaxMind.api_url.should match /https?:\/\/[\S]+/
+    end
+  end
 end
