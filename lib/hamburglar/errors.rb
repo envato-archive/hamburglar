74f1fe9f53a98228f31b581b1dfa176f19ee91b9
Joshua Priddle <jpriddle@nevercraft.net>
2011-05-25 12:49:17 -0400

    Cleanup exception message for InvalidGateway

--- lib/hamburglar/errors.rb
+++ lib/hamburglar/errors.rb
@@ -2,8 +2,10 @@ module Hamburglar
   # Raised when trying to assign an invalid gateway to
   # Hamburglar.gateway
   class InvalidGateway < StandardError
-    def initialize(gateway = '')
-      super "Invalid gateway, #{gateway}"
+    def initialize(gateway = nil)
+      msg = "Invalid gateway"
+      msg << ", #{gateway}" if gateway
+      super msg
     end
   end
 end
