b0bd8394668f7355fda8e48bf836140e0ad3385c
Joshua Priddle <jpriddle@nevercraft.net>
2011-05-25 15:33:18 -0400

    Un-Debugging

--- lib/hamburglar/gateways.rb
+++ lib/hamburglar/gateways.rb
@@ -1,6 +1,6 @@
 module Hamburglar
   module Gateways
-    autoload :Base,    'Hamburglar/gateways/base'
+    autoload :Base,    'hamburglar/gateways/base'
     autoload :MaxMind, 'hamburglar/gateways/max_mind'
   end
 end
