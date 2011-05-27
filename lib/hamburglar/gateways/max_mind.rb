ef6cbf55040025c840825eb42c044116c5386655
Joshua Priddle <jpriddle@nevercraft.net>
2011-05-27 13:10:22 -0400

    Move MaxMind class to MaxMind::MinFraud

--- lib/hamburglar/gateways/max_mind.rb
+++ lib/hamburglar/gateways/max_mind.rb
@@ -1,44 +1,52 @@
 module Hamburglar
   module Gateways
-    # The Hamburglar::Gateways::MaxMind class handles fraud verification
-    # through MaxMind's minFraud API.
-    #
-    # See: http://www.maxmind.com/app/ccv
-    class MaxMind < Base
-      PARAMS = [
-        :i,
-        :city,
-        :region,
-        :postal,
-        :country,
-        :license_key,
-        :domain,
-        :bin,
-        :binName,
-        :binPhone,
-        :custPhone,
-        :requested_type,
-        :forwardedIP,
-        :emailMD5,
-        :usernameMD5,
-        :passwordMD5,
-        :shipAddr,
-        :shipCity,
-        :shipRegion,
-        :shipPostal,
-        :shipCountry,
-        :textID,
-        :sessionID,
-        :user_agent,
-        :accept_language
-      ].freeze
+    # The MaxMind module contains classes for working
+    # with MaxMind's minFraud and Telephone Verification APIs
+    module MaxMind
+      # The MinFraud class handles fraud verification
+      # through MaxMind's minFraud API.
+      #
+      # See: http://www.maxmind.com/app/ccv
+      class MinFraud < Base
+        PARAMS = [
+          :i,
+          :city,
+          :region,
+          :postal,
+          :country,
+          :license_key,
+          :domain,
+          :bin,
+          :binName,
+          :binPhone,
+          :custPhone,
+          :requested_type,
+          :forwardedIP,
+          :emailMD5,
+          :usernameMD5,
+          :passwordMD5,
+          :shipAddr,
+          :shipCity,
+          :shipRegion,
+          :shipPostal,
+          :shipCountry,
+          :textID,
+          :sessionID,
+          :user_agent,
+          :accept_language
+        ].freeze
 
-      # The MaxMind API URL
-      # TODO: Switch to https
-      api_url "http://minfraud2.maxmind.com/app/ccv2r"
+        # The MaxMind API URL
+        # TODO: Switch to https
+        api_url "http://minfraud2.maxmind.com/app/ccv2r"
 
-      # Required parameters for a MaxMind API call
-      required_params :i, :city, :region, :postal, :country, :license_key
+        # Required parameters for a MaxMind API call
+        required_params :i, :city, :region, :postal, :country, :license_key
+      end
+
+      class TelephoneVerification < Base
+      end
+
     end
   end
 end
--- spec/hamburglar/gateways/max_mind_spec.rb
+++ spec/hamburglar/gateways/max_mind_spec.rb
@@ -1,19 +1,25 @@
 require 'spec_helper'
 
 describe Hamburglar::Gateways::MaxMind do
-  describe "::PARAMS" do
-    it { Hamburglar::Gateways::MaxMind::PARAMS.should be_an Array }
-    it { Hamburglar::Gateways::MaxMind::PARAMS.should be_frozen }
-  end
+  describe "::MinFraud" do
+    describe "::PARAMS" do
+      it { Hamburglar::Gateways::MaxMind::MinFraud::PARAMS.should be_an Array }
+      it { Hamburglar::Gateways::MaxMind::MinFraud::PARAMS.should be_frozen }
+    end
 
-  describe "::required_params" do
-    [:i, :city, :region, :postal, :country, :license_key].each do |p|
-      it { Hamburglar::Gateways::MaxMind.required_params.should include p }
+    describe "::required_params" do
+      [:i, :city, :region, :postal, :country, :license_key].each do |p|
+        it { Hamburglar::Gateways::MaxMind::MinFraud.required_params.should include p }
+      end
+    end
+
+    describe "::api_url" do
+      reg = Hamburglar::Gateways::Base::URL_REGEX
+      it { Hamburglar::Gateways::MaxMind::MinFraud.api_url.should match reg }
     end
   end
 
-  describe "::api_url" do
-    reg = Hamburglar::Gateways::Base::URL_REGEX
-    it { Hamburglar::Gateways::MaxMind.api_url.should match reg }
+  describe "::TelephoneVerification" do
+    it "has specs"
   end
 end
